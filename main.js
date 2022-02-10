7475ab645f197733669fb8926401df5374e825fb
Kaza16 <81726443+Kaza16@users.noreply.github.com>
2022-02-09 16:42:12 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function preload(){
+
+}
+function setup(){
+    canvas=createCanvas(300,300);
+    canvas.center()
+}
+function draw(){
+
+}
+function onClick(){
+    save("My_Filter_Picture.png");
+}
